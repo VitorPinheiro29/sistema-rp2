0cf494c09628d55583de28e18fa048b09269272a
Wesley Carneiro <wesley1995.silva@gmail.com>
2023-11-16 00:16:21 -0300

    added service graph

---
+++ backend/myapp/services/graph.py
@@ -0,0 +1,29 @@
+from ..models.vertex import Vertex
+from ..models.edge import Edge
+
+'''
+    Classe que representa o grafo do mapa da EACH.
+    O grafo é contruído em tempo de execução quando instanciado.
+    
+    Atributos:
+        - vertices: conjunto de vértices do grafo
+        - edges: conjunto de arestas do grafo
+    
+    Métodos:
+        - build(): constroi o grafo com base nos dados da tabela de vértices e arestas
+        - most_acessible_route(): implementa o algoritmo de Dijkstra para encontrar a rota mais acessível
+'''
+class Graph:
+    
+    def __init__(self):
+        self._vertices = set(Vertex.objects.all())
+        self._edges = set(Edge.objects.all())
+        self._build()
+    
+    def _build(self):
+        for vertex in self._vertices:
+            vertex.find_edges(self._edges)
+    
+    # Implementa Dijkstra
+    def most_acessible_route(self, origin: Vertex, destiny: Edge):
+        pass
